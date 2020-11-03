a4f1fe49fc7d01788c3993ba7d8175dcfc70962c
dd84ai <dd84ai@gmail.com>
2020-11-03 20:56:40 +0700

    Managed time

--- app/routes.py
+++ app/routes.py
@@ -13,10 +13,7 @@ from datetime import datetime
 @login_required
 def index():
     user = User.query.filter_by(username='123').first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = user.posts
     return render_template('index.html', title='Home', posts=posts)
 	
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -46,10 +43,7 @@ def before_request():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    posts = current_user.posts
     return render_template('user.html', user=user, posts=posts)	
 
 @app.route('/login', methods=['GET', 'POST'])

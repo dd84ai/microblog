e23054de1b97dc5e74c01104223cd0d2cc77f67e
dd84ai <dd84ai@gmail.com>
2020-11-03 15:19:48 +0700

    Fixed errors, working version for login and logout

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*- 
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user,  login_required
 from app.models import User
-from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -41,10 +40,8 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 	
 @app.route('/logout')
 def logout():
